7d6629badd6c23860f85c0cf624c5b4479a57035
Mohsin Maqbool <124125090+Mohsin-Maqbool@users.noreply.github.com>
2023-09-04 16:18:34 +0500

    Stopwatch Developed By Me

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var hours  = 0
+var hours = 0
 var minutes = 0
 var seconds = 0
 var miliseconds = 0
@@ -8,36 +8,36 @@ var getsec = document.getElementById('sec')
 var getmsec = document.getElementById('msec')
 
 
-function start(){
-    interval = setInterval(function(){
+function start() {
+    interval = setInterval(function () {
         miliseconds++
         getmsec.innerHTML = miliseconds
-        if(miliseconds >=100){
+        if (miliseconds >= 100) {
             seconds++
             getsec.innerHTML = seconds
             miliseconds = 0
         }
-        else if(seconds >=60){
+        else if (seconds >= 60) {
             minutes++
             getmin.innerHTML = minutes
-            seconds =  0
+            seconds = 0
         }
-        else if(minutes >=60){
+        else if (minutes >= 60) {
             hours++
             gethour.innerHTML = hours
             minutes = 0
         }
-    },10)
+    }, 10)
     document.getElementById('disable').disabled = true
 }
 
 
-function stop(){
+function stop() {
     clearInterval(interval)
     document.getElementById('disable').disabled = false
 }
 
-function reset(){
+function reset() {
     hours = 0
     minutes = 0
     seconds = 0
