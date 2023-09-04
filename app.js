0da848f8235b1a55a9aa45332ce049e06c3f9edb
Mohsin Maqbool <124125090+Mohsin-Maqbool@users.noreply.github.com>
2023-09-04 16:09:26 +0500

    Stopwatch Developed By Me

---
+++ app.js
@@ -0,0 +1,49 @@
+var hours  = 0
+var minutes = 0
+var seconds = 0
+var miliseconds = 0
+var gethour = document.getElementById('hour')
+var getmin = document.getElementById('min')
+var getsec = document.getElementById('sec')
+var getmsec = document.getElementById('msec')
+
+
+function start(){
+    interval = setInterval(function(){
+        miliseconds++
+        getmsec.innerHTML = miliseconds
+        if(miliseconds >=100){
+            seconds++
+            getsec.innerHTML = seconds
+            miliseconds = 0
+        }
+        else if(seconds >=60){
+            minutes++
+            getmin.innerHTML = minutes
+            seconds =  0
+        }
+        else if(minutes >=60){
+            hours++
+            gethour.innerHTML = hours
+            minutes = 0
+        }
+    },10)
+    document.getElementById('disable').disabled = true
+}
+
+
+function stop(){
+    clearInterval(interval)
+    document.getElementById('disable').disabled = false
+}
+
+function reset(){
+    hours = 0
+    minutes = 0
+    seconds = 0
+    miliseconds = 0
+    gethour.innerHTML = hours
+    getmin.innerHTML = minutes
+    getsec.innerHTML = seconds
+    getmsec.innerHTML = miliseconds
+}
